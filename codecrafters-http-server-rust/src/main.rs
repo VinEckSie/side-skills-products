e45fe75bee7ae0d789b0ffd23bc37cc1a6342c5e
VinEckSie <new.riverside751@passmail.net>
2025-06-23 21:34:30 +0200

    init test codecrafters

--- codecrafters-http-server-rust/src/main.rs
+++ codecrafters-http-server-rust/src/main.rs
@@ -2,11 +2,9 @@
 use std::net::TcpListener;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-
     
     for stream in listener.incoming() {
         match stream {
